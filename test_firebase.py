882a47b8f97a8c186b19481a04be8d6d7ac4373e
Nikhil Mittal <nmittal1_be22@thapar.edu>
2025-12-19 22:34:32 +0530

    change requirements.txt

--- test_firebase.py
+++ test_firebase.py
@@ -1,10 +1,16 @@
 from utils.aayu_firebase import init_db, _collection, PI_ID
-import winsound
+import time
+
 init_db()
+
 docs = _collection("medication").where("pi_id", "==", PI_ID).stream()
+
 for d in docs:
     print(d.id, d.to_dict())
+
 try:
-    winsound.Beep(1000, 200)   # frequency=1000 Hz, duration=200 ms
+    # beep simulation for non-windows
+    print("\a") 
+    time.sleep(0.2)
 except:
     print("nooo")
